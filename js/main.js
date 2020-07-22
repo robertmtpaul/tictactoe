0459454c211d6232f4ed1e41952fbf8cff0ddd62
robertmtpaul <rob@glo.id.au>
2020-07-23 09:56:48 +1000

    Added various code comments.

--- js/main.js
+++ js/main.js
@@ -1,55 +1,74 @@
 console.log('Tic Tac Toe!');
 
-// Track the number of turns a player has had, to check,  
 
-
-// 0. Turn tracker. There should be a global variable from the outset that checks... how many turns have been taken and switches to 'X' or 'O' depending on the amount
+// 0. Turn tracker. There should be a global variable from the outset that checks how many turns have been taken. 
 
 let numberOfTurns = 1;
 
-
 $(document).ready ( function() {       
-    
-    // const areAllThreeTheSame = function(input1,input2,input3) {
-    //     return input1 === input2 === input3;
-    // }
 
     const checkCombination = function () {
         let didTheyWin = false;
-        // Check if cells aren't empty, and if not, runs the checkWinningMove. 
         
         //check if the player has a horizontal row of the same item. 
-        if ($('.cell1').text() !== '' && $('.cell1').text() === $('.cell2').text() && $('.cell3').text() === $('.cell2').text() ) { 
+        if (
+            $(".cell1").text() !== "" && //        // Check if cells aren't empty, and if not, runs the checkWinningMove conditional logic.
+            $(".cell1").text() === $(".cell2").text() &&
+            $(".cell3").text() === $(".cell2").text()
+          ) {
             didTheyWin = true;
-            console.log('Top row identical')
-        } 
-        else if ( $('.cell4').text() !== '' && $('.cell4').text() === $('.cell5').text() && $('.cell5').text() === $('.cell6').text() ) {
+            console.log("Top row identical");
+          } else if (
+            $(".cell4").text() !== "" &&
+            $(".cell4").text() === $(".cell5").text() &&
+            $(".cell5").text() === $(".cell6").text()
+          ) {
             // didTheyWin = true;
-            console.log('Middle row identical')        
-        } 
-        else if ( $('.cell7').text() !== '' && $('.cell7').text() === $('.cell8').text() && $('.cell9').text() === $('.cell8').text() ) {
+            console.log("Middle row identical");
+          } else if (
+            $(".cell7").text() !== "" &&
+            $(".cell7").text() === $(".cell8").text() &&
+            $(".cell9").text() === $(".cell8").text()
+          ) {
             // didTheyWin = true;
-            console.log('Bottom row identical');        
-        }
-        //check if player has a vertical row
-        else if ( $('.cell1').text() !== '' && $('.cell1').text() === $('.cell4').text() && $('.cell7').text() === $('.cell4').text() ) {
+            console.log("Bottom row identical");
+          }
+          //check if player has a vertical row
+          else if (
+            $(".cell1").text() !== "" &&
+            $(".cell1").text() === $(".cell4").text() &&
+            $(".cell7").text() === $(".cell4").text()
+          ) {
             // didTheyWin = true;
-            console.log('Left column identical');       
-        }
-        else if ( $('.cell2').text() !== '' && $('.cell2').text() === $('.cell5').text() && $('.cell8').text() === $('.cell5').text() ) {
+            console.log("Left column identical");
+          } else if (
+            $(".cell2").text() !== "" &&
+            $(".cell2").text() === $(".cell5").text() &&
+            $(".cell8").text() === $(".cell5").text()
+          ) {
             // didTheyWin = true;
-            console.log('Middle column identical');       
-        }
-        else if ( $('.cell3').text() !== '' && $('.cell3').text() === $('.cell6').text() && $('.cell9').text() === $('.cell6').text() ) {
+            console.log("Middle column identical");
+          } else if (
+            $(".cell3").text() !== "" &&
+            $(".cell3").text() === $(".cell6").text() &&
+            $(".cell9").text() === $(".cell6").text()
+          ) {
             // didTheyWin = true;
-            console.log('Right column identical');        
-        }
-        //check if player has diagonal combo
-        else if ( $('.cell1').text() !== '' && $('.cell1').text() === $('.cell5').text() && $('.cell9').text() === $('.cell5').text() ) {
+            console.log("Right column identical");
+          }
+          //check if player has diagonal combo
+          else if (
+            $(".cell1").text() !== "" &&
+            $(".cell1").text() === $(".cell5").text() &&
+            $(".cell9").text() === $(".cell5").text()
+          ) {
             // didTheyWin = true;
-                console.log('Diagonals identical');        
-        }
-        else if ( $('.cell3').text() !== '' && $('.cell3').text() === $('.cell5').text() && $('.cell7').text() === $('.cell5').text() ) {
+            console.log("Diagonals identical");
+          } else if (
+            $(".cell3").text() !== "" &&
+            $(".cell3").text() === $(".cell5").text() &&
+            $(".cell7").text() === $(".cell5").text()
+          ) {
             // didTheyWin = true;
                 console.log('Diagonals identical');        
         }//end of conditional statements
@@ -58,23 +77,28 @@ $(document).ready ( function() {
 
     $('.cell').click(function(){            
         console.log('cell clicked!');
-        if (numberOfTurns % 2 === 0){
-            // Tell the user that it's player 1's turn.
-            $('#gameMessage').html("It's Player 1's turn");
-            //Adds X to cell after click event.
-            $(this).html('X');
-            numberOfTurns += 1;
-            checkCombination();  
-        } else {
-            //Tell the user that it's player 2's turn.
-            $('#gameMessage').html("It's Player 2's turn");
-            //Add 'O' mark to cell after click event.
-            $(this).html('O');
-            numberOfTurns += 1;
-            checkCombination();
-        }
+            if ($(this).text() === '') {
+                if (numberOfTurns % 2 === 0){
+                    // Tell the user that it's player 1's turn.
+                    $('#gameMessage').html("It's Player 1's turn");
+                    //Adds X to cell after click event.
+                    $(this).html('X');
+                    numberOfTurns += 1;
+                    checkCombination();  
+                } else {
+                    //Tell the user that it's player 2's turn.
+                    $('#gameMessage').html("It's Player 2's turn");
+                    //Add 'O' mark to cell after click event.
+                    $(this).html('O');
+                    numberOfTurns += 1;
+                    checkCombination();
+                }
+            }
     }); // jQuery - click function.
  
+
+
+
     
 //3. When the player manages to have 3 'X' or 'O' in a row, or diagonally, they should win the round.
     // --> a function could check whether there are winning moves in the grid.
