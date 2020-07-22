3989e6e0ccc1cc9304216fea85e030a1fab96955
robertmtpaul <rob@glo.id.au>
2020-07-22 16:24:47 +1000

    Beginning of fixes to conditional logic to check if box is empty string; added reset button that removes all values from board.

--- js/main.js
+++ js/main.js
@@ -7,96 +7,90 @@ console.log('Tic Tac Toe!');
 
 let numberOfTurns = 1;
 
+
 $(document).ready ( function() {       
     
     // const areAllThreeTheSame = function(input1,input2,input3) {
     //     return input1 === input2 === input3;
     // }
 
-    const checkCombination = function (input) {
-        //if the box isn't empty, then check for a winner ... 
-        if ( $('.cell') !== '') {
-        //check if the player has a horizontal row of the same item. 
-            if ( $('.cell1').text() === $('.cell2').text() && $('.cell3').text() === $('.cell2').text() ) { 
-                console.log('Player won')
-            } 
-            else if ( $('.cell4').text() && $('.cell5').text() && $('.cell6').text() ) {
-            console.log('Player won')        
-            } 
-            else if ( $('.cell7').text() && $('.cell8').text() && $('.cell9').text() ) {
-                return true;
-                console.log('Player won')        
-            }
-        //check if player has a vertical row
-            else if ( $('.cell1').text() && $('.cell4').text() && $('.cell7').text() ) {
-                return true;
-                console.log('Player won')        
-            }
-            else if ( $('.cell2').text() && $('.cell5').text() && $('.cell8').text() ) {
-                return true;
-                console.log('Player won')        
-            }
-            else if ( $('.cell3').text() && $('.cell6').text() && $('.cell9').text() ) {
-                return true;
-                console.log('Player won')        
-            }
-        //check if player has diagonal combo
-            else if ( $('.cell1').text() && $('.cell5').text() && $('.cell9').text() ) {
-                return true;
-                console.log('Player won')        
-            }
-            else if ( $('.cell3').text() && $('.cell5').text() && $('.cell7').text() ) {
-                return true;
-                console.log('Player won')        
-            }
-        } // end of parent if statement
-
-
+    const checkCombination = function () {
+        let didTheyWin = false;
+        // Check if cells aren't empty, and if not, runs the checkWinningMove. 
         
+        //check if the player has a horizontal row of the same item. 
+        if ($('.cell1').text() !== '' && $('.cell1').text() === $('.cell2').text() && $('.cell3').text() === $('.cell2').text() ) { 
+            didTheyWin = true;
+        } 
+        else if ( $('.cell4').text() === $('.cell5').text() && $('.cell5').text() === $('.cell6').text() ) {
+            // didTheyWin = true;
+            console.log('Middle row identical')        
+        } 
+        else if ( $('.cell7').text() === $('.cell8').text() && $('.cell9').text() === $('.cell8').text() ) {
+            // didTheyWin = true;
+            console.log('Bottom row identical');        
+        }
 
-        } //check combination.
+        //check if player has a vertical row
+        else if ( $('.cell1').text() === $('.cell4').text() && $('.cell7').text() === $('.cell4').text() ) {
+            // didTheyWin = true;
+            console.log('Left row identical');       
+        }
+        else if ( $('.cell2').text() === $('.cell5').text() && $('.cell8').text() === $('.cell5').text() ) {
+            // didTheyWin = true;
+            console.log('Middle column identical');       
+        }
+        else if ( $('.cell3').text() === $('.cell6').text() && $('.cell9').text() === $('.cell6').text() ) {
+            // didTheyWin = true;
+            console.log('Right column identical');        
+        }
+        //check if player has diagonal combo
+        else if ( $('.cell1').text() === $('.cell5').text() && $('.cell9').text() === $('.cell5').text() ) {
+        // didTheyWin = true;
+                console.log('Diagonals identical');        
+        }
+        else if ( $('.cell3').text() === $('.cell5').text() && $('.cell7').text() === $('.cell5').text() ) {
+        // didTheyWin = true;
+                console.log('Diagonals identical');        
+        }//end of conditional statements
+    } //end of checkCombination function.
 
-// if cells aren't empty, then run the checkWinningMove. 
 
     $('.cell').click(function(){            
         console.log('cell clicked!');
-        if(numberOfTurns % 2 === 0) {
+        if( (numberOfTurns % 2 === 0) && $(this).val === '')){
             // Tell the user that it's player 1's turn.
             $('#gameMessage').html("It's Player 1's turn");
-            //Adds circle to cell after click event.
+            //Adds X to cell after click event.
             $(this).html('X');
             numberOfTurns += 1;
-            checkCombination();
+            checkCombination();  
         } else {
             //Tell the user that it's player 2's turn.
             $('#gameMessage').html("It's Player 2's turn");
-            //Add 'O' mark to cell in grid.
+            //Add 'O' mark to cell after click event.
             $(this).html('O');
             numberOfTurns += 1;
             checkCombination();
         }
-        
     }); // jQuery - click function.
-    
-    // RESET BUTTON:
-    //  reset all cell divs to have no HTML
-    // target all divs with cell class and give zero value with .html jqueryll..
-    
+ 
     
 //3. When the player manages to have 3 'X' or 'O' in a row, or diagonally, they should win the round.
     // --> a function could check whether there are winning moves in the grid.
 
-
-
-
 // 4. The game should finish when player wins, and keep track of how many times the player has won.
         // --> global variable for number of wins
         // --> the game should keep adding to the variable depending on the outcome of the game.
-// 5. 
+// 5. There should be a button to clear the grid and play counters
+    
+    // RESET BUTTON:
+    //  reset all cell divs to have no HTML
+    $('#button').click(function(){
+        // console.log('clear game');
+        $('.cell').html('');
 
-
-
-
+    });   
 }); //end jQuery - document.ready
 
 
