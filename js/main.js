f88b647ef174391eb30a904fec4e187e059fa241
robertmtpaul <rob@glo.id.au>
2020-07-23 10:18:00 +1000

    Finished writing comments for most game functions.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 console.log('Tic Tac Toe!');
 
-
-// 0. Turn tracker. There should be a global variable from the outset that checks how many turns have been taken. 
+//Turn tracker: global variable that tracks from the outset how many turns have been taken. 
 
 let numberOfTurns = 1;
 
@@ -14,16 +13,16 @@ $(document).ready ( function() {    //Start of jQuery DOM handling.
         let didTheyWin = false; 
         
         if ( 
-            // Check if cells aren't empty, and if not, runs the following conditional logic
+            // Checks if cells aren't empty, and if not, runs the following conditional logic
             $(".cell1").text() !== "" && 
-            //Check if the player has a horizontal top row of the same item.        
+            //Checks if the player has a horizontal top row of the same item.        
             $(".cell1").text() === $(".cell2").text() &&
             $(".cell3").text() === $(".cell2").text()
           ) {
             didTheyWin = true;
             console.log("Top row identical");
           } else if (
-            //Check if the player has a horizontal middle row of the same item.
+            //Checks if the player has a horizontal middle row of the same item.
             $(".cell4").text() !== "" &&
             $(".cell4").text() === $(".cell5").text() &&
             $(".cell5").text() === $(".cell6").text()
@@ -31,16 +30,15 @@ $(document).ready ( function() {    //Start of jQuery DOM handling.
             didTheyWin = true;
             console.log("Middle row identical");
           } else if (
-            //Check if the player has a horizontal bottom row of the same item.
+            //Checks if the player has a horizontal bottom row of the same item.
             $(".cell7").text() !== "" &&
             $(".cell7").text() === $(".cell8").text() &&
             $(".cell9").text() === $(".cell8").text()
           ) {
             didTheyWin = true;
             console.log("Bottom row identical");
-          }
-          //Check if the player has a horizontal bottom row of the same item.
-          else if (
+          } else if (
+            //Checks if the player has left column of the same item.
             $(".cell1").text() !== "" &&
             $(".cell1").text() === $(".cell4").text() &&
             $(".cell7").text() === $(".cell4").text()
@@ -48,6 +46,7 @@ $(document).ready ( function() {    //Start of jQuery DOM handling.
             didTheyWin = true;
             console.log("Left column identical");
           } else if (
+            //Checks if the player has middle column of the same item.
             $(".cell2").text() !== "" &&
             $(".cell2").text() === $(".cell5").text() &&
             $(".cell8").text() === $(".cell5").text()
@@ -55,6 +54,7 @@ $(document).ready ( function() {    //Start of jQuery DOM handling.
             didTheyWin = true;
             console.log("Middle column identical");
           } else if (
+            //Checks if the player has right column of the same item.
             $(".cell3").text() !== "" &&
             $(".cell3").text() === $(".cell6").text() &&
             $(".cell9").text() === $(".cell6").text()
@@ -62,8 +62,8 @@ $(document).ready ( function() {    //Start of jQuery DOM handling.
             didTheyWin = true;
             console.log("Right column identical");
           }
-          //check if player has diagonal combo
           else if (
+            //Checks if there is a diagonal combination of same item.
             $(".cell1").text() !== "" &&
             $(".cell1").text() === $(".cell5").text() &&
             $(".cell9").text() === $(".cell5").text()
@@ -71,6 +71,7 @@ $(document).ready ( function() {    //Start of jQuery DOM handling.
             didTheyWin = true;
             console.log("Diagonals identical");
           } else if (
+            //Checks if there is a diagonal combination of same item.
             $(".cell3").text() !== "" &&
             $(".cell3").text() === $(".cell5").text() &&
             $(".cell7").text() === $(".cell5").text()
@@ -84,33 +85,28 @@ $(document).ready ( function() {    //Start of jQuery DOM handling.
         console.log('cell clicked!');
         if ($(this).text() === '') {
             if (numberOfTurns % 2 === 0){
-                // Tell the user that it's player 1's turn.
+                // Show whose turn it is.
                 $('#gameMessage').html("It's Player 1's turn");
-                //Adds X to cell after click event.
+                //Add X to cell after click event.
                 $(this).html('X');
-                numberOfTurns += 1;
+                //Check if the move has won the game.
                 checkCombination();  
+                //Switch turn to next player
+                numberOfTurns += 1;
             } else {
-                //Tell the user that it's player 2's turn.
+                // Show whose turn it is.
                 $('#gameMessage').html("It's Player 2's turn");
                 //Add 'O' mark to cell after click event.
                 $(this).html('O');
-                numberOfTurns += 1;
+                //Check if the move has won the game.
                 checkCombination();
+                //Switch turn to next player
+                numberOfTurns += 1;
             }
         }
     }); // jQuery - click function.
     
-//3. When the player manages to have 3 'X' or 'O' in a row, or diagonally, they should win the round.
-    // --> a function could check whether there are winning moves in the grid.
-
-// 4. The game should finish when player wins, and keep track of how many times the player has won.
-        // --> global variable for number of wins
-        // --> the game should keep adding to the variable depending on the outcome of the game.
-// 5. There should be a button to clear the grid and play counters
-    
-    // RESET BUTTON:
-    //  reset all cell divs to have no HTML
+    // RESET BUTTON: resets all cell divs to have no HTML so that players can restart.
     $('#button').click(function(){
         // console.log('clear game');
         $('.cell').html('');
