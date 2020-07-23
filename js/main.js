e63be292672f6855182711c050110cf9600d185d
robertmtpaul <rob@glo.id.au>
2020-07-23 17:34:13 +1000

    Fully refactored conditional statements to be more efficient

--- js/main.js
+++ js/main.js
@@ -3,140 +3,120 @@ console.log('Tic Tac Toe!');
 //Turn tracker: global variable that tracks from the outset how many turns have been taken. 
 
 let numberOfTurns = 0;
-let didTheyWin = false; 
+let didTheyWin = false;
 
-$(document).ready ( function() {    //Start of jQuery DOM handling.   
+$(document).ready(function () {    //Start of jQuery DOM handling.   
 
-    //Function to run to check with conditional logic whether combination of moves wins the game. 
-    const checkCombination = function () { 
-        let cell1 = $(".cell1").html();
-        let cell2 = $(".cell2").html();
-        let cell3 = $(".cell3").html();
-        let cell4 = $(".cell4").html();
-        let cell5 = $(".cell5").html();
-        let cell6 = $(".cell6").html();
-        let cell7 = $(".cell7").html();
-        let cell8 = $(".cell8").html();
-        let cell9 = $(".cell9").html();
-        
-        // Check if cells aren't empty, and if not, runs conditional logic
-        if (cell1 !== "" && cell1 === cell2 && cell3 === cell2) {
-            didTheyWin = true;
-            console.log("Top row identical");
-            numberOfTurns = 0;
-          } //Check if the player has a horizontal middle row of the same item. 
-        else if (cell4 !== "" && cell4 === cell5 && cell5 === cell6 ){
-            didTheyWin = true;
-            console.log("Middle row identical");
-            numberOfTurns = 0;
-        } //Checks if the player has a horizontal bottom row of the same item.
-        else if ( cell7 !== "" && cell7 === cell8 && cell9 === cell8 ) {
-            didTheyWin = true;
-            console.log("Bottom row identical");
-            $('.cell').html('');
-            numberOfTurns = 0;          
-        } else if (cell1 !== "" && cell1 === $(".cell4").text() && $(".cell7").text() === $(".cell4").text()
+  //Function to run to check with conditional logic whether combination of moves wins the game. 
+  const checkCombination = function () {
+    let cell1 = $(".cell1").html();
+    let cell2 = $(".cell2").html();
+    let cell3 = $(".cell3").html();
+    let cell4 = $(".cell4").html();
+    let cell5 = $(".cell5").html();
+    let cell6 = $(".cell6").html();
+    let cell7 = $(".cell7").html();
+    let cell8 = $(".cell8").html();
+    let cell9 = $(".cell9").html();
 
-            //Checks if the player has left column of the same item.
-            
-          ) {
-            didTheyWin = true;
-            console.log("Left column identical");
-            numberOfTurns = 0;
-          } else if (
-            //Checks if the player has middle column of the same item.
-            $(".cell2").text() !== "" &&
-            $(".cell2").text() === $(".cell5").text() &&
-            $(".cell8").text() === $(".cell5").text()
-          ) {
-            didTheyWin = true;
-            console.log("Middle column identical");
-            numberOfTurns = 0;
-          } else if (
-            //Checks if the player has right column of the same item.
-            $(".cell3").text() !== "" &&
-            $(".cell3").text() === $(".cell6").text() &&
-            $(".cell9").text() === $(".cell6").text()
-          ) {
-            didTheyWin = true;
-            console.log("Right column identical");
-            numberOfTurns = 0;
-          }
-          else if (
-            //Checks if there is a diagonal combination of same item.
-            cell1 !== "" &&
-            cell1 === $(".cell5").text() &&
-            $(".cell9").text() === $(".cell5").text()
-          ) {
-            didTheyWin = true;
-            console.log("Diagonals identical");
-            numberOfTurns = 0;
 
-          } else if (
-            //Checks if there is a diagonal combination of same item.
-            $(".cell3").text() !== "" &&
-            $(".cell3").text() === $(".cell5").text() &&
-            $(".cell7").text() === $(".cell5").text()
-          ) { 
-            didTheyWin = true;
-            console.log('Diagonals identical');
-            numberOfTurns = 0;
-        } else if ( numberOfTurns >= 9 ) {
-            //Checks if all turns have been taken, and tells the players it's a draw.
-            console.log("It's a draw");
-            $('#dialog span').html("It's a draw. Play again!");
-            $('#dialog').show();
-            numberOfTurns = 0;
-        }        
-        //end of conditional statements
-    } //end of checkCombination function.
+    if ( cell1 !== "" && cell1 === cell2 && cell3 === cell2 ) {
+      // Check if cells aren't empty, and if not, runs conditional logic
+      didTheyWin = true;
+      console.log("Top row identical");
+      numberOfTurns = 0;
+    } else if ( cell4 !== "" && cell4 === cell5 && cell5 === cell6 ) {
+      //Check if the player has a horizontal middle row of the same item. 
+      didTheyWin = true;
+      console.log("Middle row identical");
+      numberOfTurns = 0;
+    } else if ( cell7 !== "" && cell7 === cell8 && cell9 === cell8 ) {
+      //Checks if the player has a horizontal bottom row of the same item.
+      didTheyWin = true;
+      console.log("Bottom row identical");
+      $('.cell').html('');
+      numberOfTurns = 0;
+    } else if ( cell1 !== "" && cell1 === cell4 && cell7 === cell4 ) {
+      //Checks if the player has left column of the same item.
+      didTheyWin = true;
+      console.log("Left column identical");
+      numberOfTurns = 0;
+    } else if ( cell2 !== "" && cell2 === cell5 && cell8 === cell5 ) {
+      //Checks if the player has middle column of the same item.
+      didTheyWin = true;
+      console.log("Middle column identical");
+      numberOfTurns = 0;
+    } else if ( cell3 !== "" && cell3 === cell6 && cell9 === cell6 ) {
+      //Checks if the player has right column of the same item.
+      didTheyWin = true;
+      console.log("Right column identical");
+      numberOfTurns = 0;
+    } else if ( cell1 !== "" && cell1 === cell5 && cell9 === cell5 ) {
+      //Checks if there is a diagonal combination of same item.
+      didTheyWin = true;
+      console.log("Diagonals identical");
+      numberOfTurns = 0;
+    } else if ( cell3 !== "" && cell3 === cell5 && cell7 === cell5 ) {
+      //Checks if there is a diagonal combination of same item.
+      didTheyWin = true;
+      console.log('Diagonals identical');
+      numberOfTurns = 0;
+    } else if (numberOfTurns >= 9) {
+      //Checks if all turns have been taken, and tells the players it's a draw.
+      console.log("It's a draw");
+      $('#dialog span').html("It's a draw. Play again!");
+      $('#dialog').show();
+      numberOfTurns = 0;
+    }
+    //end of conditional statements
+  } //end of checkCombination function.
 
-    $('.cell').click(function(){            
-        console.log('cell clicked!');
-        if ($(this).html() === '') {
-            if (numberOfTurns % 2 === 0){
-                //Adds X to cell after click event.
-                $(this).html('<i class="fas fa-hands-wash"></i>');
-                //Switches turn to next player
-                numberOfTurns += 1;
-                //Checks if the move has won the game.
-                checkCombination();  
-                if ( didTheyWin === true ) {
-                  $('#dialog span').html('Player <i class="fas fa-hands-wash"></i> wins.');
-                  $('#dialog').show();
-                }
-                // get function to return true/false value of checkCom.. to indicate win
-                console.log(numberOfTurns);
-            } else {
-                //Add 'O' mark to cell after click event.
-                $(this).html('<i class="fas fa-virus"></i>');
-                //Switches turn to next player
-                numberOfTurns += 1;
-                console.log(numberOfTurns)
-                //Checks if the move has won the game.
-                checkCombination();
-                if ( didTheyWin === true ) {
-                  $('#dialog span').html('Player O wins.');
-                  $('#dialog').show();
-                }
-
-            }
+  $('.cell').click(function () {
+    console.log('cell clicked!');
+    if ($(this).html() === '') {
+      if (numberOfTurns % 2 === 0) {
+        //Adds X to cell after click event.
+        $(this).html('<i class="fas fa-hands-wash"></i>');
+        //Switches turn to next player
+        numberOfTurns += 1;
+        //Checks if the move has won the game.
+        checkCombination();
+        if (didTheyWin === true) {
+          $('#dialog span').html('Player <i class="fas fa-hands-wash"></i> wins.');
+          $('#dialog').show();
         }
-    }); // jQuery - click function.
-    
-    // RESET BUTTON: resets all cell divs to have no HTML so that players can restart.
-    $('#button').click(function(){
-        // console.log('clear game');
-        $('.cell').html('');
-        numberOfTurns = 0;
-    });   
+        // get function to return true/false value of checkCom.. to indicate win
+        console.log(numberOfTurns);
+      } else {
+        //Add 'O' mark to cell after click event.
+        $(this).html('<i class="fas fa-virus"></i>');
+        //Switches turn to next player
+        numberOfTurns += 1;
+        console.log(numberOfTurns)
+        //Checks if the move has won the game.
+        checkCombination();
+        if (didTheyWin === true) {
+          $('#dialog span').html('Player O wins.');
+          $('#dialog').show();
+        }
 
-    $('#dialog').click(function(){
-        // console.log('close dialog');
-        numberOfTurns = 0;
-        $('.cell').html('');
-        $('#dialog').hide();
-    });            
+      }
+    }
+  }); // jQuery - click function.
+
+  // RESET BUTTON: resets all cell divs to have no HTML so that players can restart.
+  $('#button').click(function () {
+    // console.log('clear game');
+    $('.cell').html('');
+    numberOfTurns = 0;
+  });
+
+  $('#dialog').click(function () {
+    // console.log('close dialog');
+    numberOfTurns = 0;
+    $('.cell').html('');
+    $('#dialog').hide();
+  });
 
 }); //end jQuery - document.ready
 
